8246acde82214e505279dbb21a336a5fabd53c99
AlekseyLeshko <alekseyleshko@gmail.com>
2021-10-10 18:16:37 +0800

    Add the diff limit step in the CI porcess

--- example.js
+++ example.js
@@ -1,7 +1,12 @@
 const { linesControl, CheckType } = require('./dist/index')
 
 const checks = [{
-  type: CheckType.totalInsertions,
-  maxNumber: 4000,
+  type: CheckType.total,
+  maxNumber: 0,
 }];
-console.log(linesControl(checks));
+
+if (linesControl(checks)) {
+  console.log('All right!');
+} else {
+  console.log('This repo would like a smaller update');
+}
