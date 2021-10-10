67ee048a1684e3a6a0d87ba8486342a3c1fa7ce7
AlekseyLeshko <alekseyleshko@gmail.com>
2021-10-10 13:47:53 +0800

    Add the total check

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export const diffCheck = () => {
-  return true;
+type FileChange = {
+  added: number,
+  deleted: number,
+  path: string,
+}
+
+const getTotal = (change: FileChange) => change.added + change.deleted;
+
+export const linesControl = (total: number, changes: FileChange[] = []) => {
+  if (!total) return true;
+
+  return changes.reduce((acc, change) => acc += getTotal(change), 0) <= total;
 }
--- src/tests/lines-control.test.ts
+++ src/tests/lines-control.test.ts
@@ -1,7 +1,72 @@
-import { diffCheck } from '../index'
+import { linesControl } from '../index'
 
 describe('lines control', () => {
-  test('shoul return true', () => {
-    expect(diffCheck()).toBe(true);
+  test.each([
+    [
+      'total check when no value is set',
+      {
+        total: undefined,
+        changes: undefined,
+        expected: true,
+      },
+    ],
+    [
+      'total check when the total is 0',
+      {
+        total: 0,
+        changes: [],
+        expected: true,
+      },
+    ],
+    [
+      'total check when a file has changes less then the total',
+      {
+        total: 10,
+        changes: [{
+          added: 9,
+          deleted: 0,
+          path: 'x',
+        }],
+        expected: true,
+      },
+    ],
+    [
+      'total check when the file has changes equal to the total number of',
+      {
+        total: 10,
+        changes: [{
+          added: 10,
+          deleted: 0,
+          path: 'x',
+        }],
+        expected: true,
+      },
+    ],
+    [
+      'total check when a file has more changes than the total number of',
+      {
+        total: 10,
+        changes: [{
+          added: 11,
+          deleted: 0,
+          path: 'x',
+        }],
+        expected: false,
+      },
+    ],
+    [
+      'total check when the file has changes equal to the total number of',
+      {
+        total: 10,
+        changes: [{
+          added: 1,
+          deleted: 9,
+          path: 'x',
+        }],
+        expected: true,
+      },
+    ],
+  ])('should return a status for %s', (_, { total, changes, expected }) => {
+    expect(linesControl(total, changes)).toBe(expected);
   });
 });
