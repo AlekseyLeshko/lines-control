56bdde5e8aa938496e6d127b44a01ba454f82ecd
AlekseyLeshko <alekseyleshko@gmail.com>
2021-10-10 17:37:34 +0800

    Rework tests

--- src/tests/lines-control.test.ts
+++ src/tests/lines-control.test.ts
@@ -3,146 +3,152 @@ import * as childProcess from 'child_process';
 import { linesControl, CheckType } from '../index'
 
 describe('lines control', () => {
-  test.each([
-    [
-      'total check when no value is set',
-      {
-        checks: undefined,
-        changes: undefined,
-        expected: true,
-      },
-    ],
-    [
-      'total check when the total is 0',
-      {
-        checks: [{
-          type: CheckType.total,
-          maxNumber: 0,
-        }],
-        changes: [],
-        expected: true,
-      },
-    ],
-    [
-      'total check when a file has changes less then the total',
-      {
-        checks: [{
-          type: CheckType.total,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 9,
-          deletions: 0,
-          path: 'x',
-        }],
-        expected: true,
-      },
-    ],
-    [
-      'total check when the file has changes equal to the total number of',
-      {
-        checks: [{
-          type: CheckType.total,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 10,
-          deletions: 0,
-          path: 'x',
-        }],
-        expected: true,
-      },
-    ],
-    [
-      'total check when a file has more changes than the total number of',
-      {
-        checks: [{
-          type: CheckType.total,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 11,
-          deletions: 0,
-          path: 'x',
-        }],
-        expected: false,
-      },
-    ],
-    [
-      'total check when the file has changes equal to the total number of',
-      {
-        checks: [{
-          type: CheckType.total,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 1,
-          deletions: 9,
-          path: 'x',
-        }],
-        expected: true,
-      },
-    ],
-    [
-      'total check when the file has insertion changes less to the total number of',
-      {
-        checks: [{
-          type: CheckType.totalInsertions,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 1,
-          deletions: 11,
-          path: 'x',
-        }],
-        expected: true,
-      },
-    ],
-    [
-      'total check when the file has insertion changes equal to the total number of',
-      {
-        checks: [{
-          type: CheckType.totalInsertions,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 10,
-          deletions: 11,
-          path: 'x',
-        }],
-        expected: true,
-      },
-    ],
-    [
-      'total check when the file has insertion changes more to the total number of',
-      {
-        checks: [{
-          type: CheckType.totalInsertions,
-          maxNumber: 10,
-        }],
-        changes: [{
-          insertions: 11,
-          deletions: 11,
-          path: 'x',
-        }],
-        expected: false,
-      },
-    ],
-  ])('should return a status for %s', (_, { checks, total, changes, expected }) => {
-    const gitOutput = generateGitOutput(changes);
-    childProcess.execSync.mockImplementation(() => gitOutput);
+  beforeEach(() => {
+    childProcess.execSync.mockReset();
+  });
 
-    const actual = linesControl(checks);
+  describe('checks', () => {
+    test.each([
+      [
+        'total check when no value is set',
+        {
+          checks: undefined,
+          changes: undefined,
+          expected: true,
+        },
+      ],
+      [
+        'total check when the total is 0',
+        {
+          checks: [{
+            type: CheckType.total,
+            maxNumber: 0,
+          }],
+          changes: [],
+          expected: true,
+        },
+      ],
+      [
+        'total check when a file has changes less then the total',
+        {
+          checks: [{
+            type: CheckType.total,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 9,
+            deletions: 0,
+            path: 'x',
+          }],
+          expected: true,
+        },
+      ],
+      [
+        'total check when the file has changes equal to the total number of',
+        {
+          checks: [{
+            type: CheckType.total,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 10,
+            deletions: 0,
+            path: 'x',
+          }],
+          expected: true,
+        },
+      ],
+      [
+        'total check when a file has more changes than the total number of',
+        {
+          checks: [{
+            type: CheckType.total,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 11,
+            deletions: 0,
+            path: 'x',
+          }],
+          expected: false,
+        },
+      ],
+      [
+        'total check when the file has changes equal to the total number of',
+        {
+          checks: [{
+            type: CheckType.total,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 1,
+            deletions: 9,
+            path: 'x',
+          }],
+          expected: true,
+        },
+      ],
+      [
+        'total check when the file has insertion changes less to the total number of',
+        {
+          checks: [{
+            type: CheckType.totalInsertions,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 1,
+            deletions: 11,
+            path: 'x',
+          }],
+          expected: true,
+        },
+      ],
+      [
+        'total check when the file has insertion changes equal to the total number of',
+        {
+          checks: [{
+            type: CheckType.totalInsertions,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 10,
+            deletions: 11,
+            path: 'x',
+          }],
+          expected: true,
+        },
+      ],
+      [
+        'total check when the file has insertion changes more to the total number of',
+        {
+          checks: [{
+            type: CheckType.totalInsertions,
+            maxNumber: 10,
+          }],
+          changes: [{
+            insertions: 11,
+            deletions: 11,
+            path: 'x',
+          }],
+          expected: false,
+        },
+      ],
+    ])('should return a status for %s', (_, { checks, total, changes, expected }) => {
+      const gitOutput = generateGitOutput(changes);
+      const { execSync } = childProcess;
+      execSync.mockImplementation(() => gitOutput);
 
-    expect(childProcess.execSync).toHaveBeenCalled();
-    expect(actual).toBe(expected);
+      // Atc
+      const actual = linesControl(checks);
+
+      // Asserts
+      expect(execSync).toHaveBeenCalledTimes(1);
+
+      expect(actual).toBe(expected);
+    });
   });
 
   describe('diff from X to Y', () => {
-    beforeEach(() => {
-      childProcess.execSync.mockReset();
-    });
-
     test.each([
       [
         'the main and the current branch',
