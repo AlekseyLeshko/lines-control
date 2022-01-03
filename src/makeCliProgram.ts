f36ee80aacaf60ee640a94a1c11a311509a646de
AlekseyLeshko <alekseyleshko@gmail.com>
2022-01-03 11:45:45 +0800

    Fix bug

---
+++ src/makeCliProgram.ts
@@ -0,0 +1,83 @@
+import { execSync } from 'child_process';
+import commander, { Command } from 'commander';
+import isLinesControlPass, { RuleType, Rule, linesControl } from './index';
+
+const { version } = require('../package.json');
+
+export function makeCliProgram() {
+  const humanableCheckType = {
+    [RuleType[RuleType.total]]: RuleType[RuleType.total],
+    [RuleType[RuleType.totalInsertions]]: RuleType[RuleType.totalInsertions],
+  };
+
+  const rulesDescription = `Rules for checking.\nTypes: ${Object.keys(humanableCheckType).join(', ')};\nexample: total;50;src/**/*`;
+
+  const convertArgToRule = (stringArg: string) => {
+    const attrs = stringArg.split(',');
+    const checkTypeStr = (attrs[0] as unknown as RuleType);
+
+    return {
+      type: (checkTypeStr || RuleType.totalInsertions),
+      maxNumber: parseInt(attrs[1] || '0', 10),
+      pattern: attrs[2],
+    };
+  };
+
+  const addRule = (value: string, previous: Rule[]) => {
+    const rule = convertArgToRule(value);
+    return previous.concat([rule])
+  }
+
+  const addComparisons = (value: string) => {
+    const arr = value.split(',');
+
+    if (arr.length > 2) {
+      throw new commander.InvalidArgumentError('Two comparators at most.');
+    }
+
+    return arr;
+  }
+
+  const program = new Command();
+  program
+    .option('-r, --rules <value...>', rulesDescription, addRule, [])
+    .option('-c, --comparisons <value>', 'Comparison of commits and branches', addComparisons)
+    .version(version, '-v, --vers', 'output the current version')
+    .showSuggestionAfterError()
+    program.addHelpText('after', `
+    Example call:
+      $ lines-control --rules total,25 totalInsertions,5,src/**/*
+      $ lines-control --rules total,25 --comparisons master,feature/test-branch-name
+      $ lines-control --rules total,25 --comparisons main
+    `);
+
+  program.parse(process.argv);
+
+  const convertComparisonsFromArrToObj = (arr?: string[]) => {
+    if (!arr) {
+      return { to: getDefaultBranch() };
+    }
+
+    if (arr.length === 2) {
+      return {
+        from: arr[0],
+        to: arr[1],
+      };
+    }
+
+    return {
+      to: arr[0],
+    };
+  }
+
+  const options = program.opts();
+  const comparisons = convertComparisonsFromArrToObj(options.comparisons);
+
+  linesControl(options.rules, comparisons);
+}
+
+const getDefaultBranch = () => {
+  const cmd = '[ -f $(git rev-parse --show-toplevel)/.git/refs/heads/master ] && echo master || echo main';
+  const defaultBranch = execSync(cmd).toString();
+  return defaultBranch;
+}
---
+++ src/tests/cli.test.ts
@@ -0,0 +1,72 @@
+jest.mock('child_process')
+import * as cp from 'child_process';
+import { makeCliProgram } from '../makeCliProgram';
+
+describe('lines control:cli', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  test('should work without any attributes', async () => {
+    process.exit = jest.fn();
+    console.error = jest.fn();
+    console.log = jest.fn();
+
+    const defaultBranch = 'main';
+    const cmdToGetStat = `git diff ${defaultBranch} --numstat`;
+    cp.execSync.mockReturnValueOnce(defaultBranch);
+
+    const gitOutput = generateGitOutput();
+    cp.execSync.mockImplementation(() => gitOutput);
+
+    makeArgv();
+
+    // Act
+    const result = await makeCliProgram();
+
+    // Asserts
+    expect(process.exit).toHaveBeenCalledTimes(0);
+    expect(console.error).toHaveBeenCalledTimes(0);
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log).toHaveBeenCalledWith('All right!');
+    expect(cp.execSync).toHaveBeenCalledTimes(2);
+    expect(cp.execSync).toHaveBeenLastCalledWith(cmdToGetStat);
+  });
+
+  test('should work with the master branch as default', async () => {
+    process.exit = jest.fn();
+    console.error = jest.fn();
+    console.log = jest.fn();
+
+    const defaultBranch = 'master';
+    const cmdToGetStat = `git diff ${defaultBranch} --numstat`;
+    cp.execSync.mockReturnValueOnce(defaultBranch);
+
+    const gitOutput = generateGitOutput();
+    cp.execSync.mockImplementation(() => gitOutput);
+
+    makeArgv();
+
+    // Act
+    const result = await makeCliProgram();
+
+    // Asserts
+    expect(process.exit).toHaveBeenCalledTimes(0);
+    expect(console.error).toHaveBeenCalledTimes(0);
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log).toHaveBeenCalledWith('All right!');
+    expect(cp.execSync).toHaveBeenCalledTimes(2);
+    expect(cp.execSync).toHaveBeenLastCalledWith(cmdToGetStat);
+  });
+});
+
+const generateGitOutput = (changes = []) =>
+  changes.map(change => `${change.insertions}\t${change.deletions}\t${change.path}`).join('\n');
+
+const makeArgv = (argv: string[] = []) => {
+  const originalArgv = [...process.argv];
+  process.argv = [
+    originalArgv[0],
+    originalArgv[1],
+  ];
+}
